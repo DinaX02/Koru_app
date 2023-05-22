edcbf8d23e06e088b7e2a4dc15b5b698e3714ab3
DinaX <dinisantunes@ua.pt>
2023-05-22 20:59:50 +0100

    menu codigo comentado

--- components/MenuFooter.js
+++ components/MenuFooter.js
@@ -1,31 +1,20 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
-  
 
-const FooterMenu = ({navigation}) => {
+const FooterMenu = () => {
   const [activeTab, setActiveTab] = useState('home');
-
 
   const handleTabPress = (tab) => {
     setActiveTab(tab);
-
-    // if (tab === 'home') {
-    //   navigation.navigate('Homepage');   {/*  onPress={() => handleTabPress('home')} */}
-    // } else if (tab === 'list') {
-    //   navigation.navigate('List');
-    // } else if (tab === 'profile') {    {/* onPress={() => handleTabPress('profile')}*/}
-    //   navigation.navigate('Profile');
-    // }   
+    // Adicione aqui a lógica para navegar para a página correspondente
   };
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.tab}
-   
-      >   
-
-
+        onPress={() => handleTabPress('home')}
+      >
         <Image
           source={require('../assets/home.png')}
           style={[
@@ -50,11 +39,8 @@ const FooterMenu = ({navigation}) => {
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.tab}
-       
+        onPress={() => handleTabPress('profile')}
       >
-
-
-
         <Image
           source={require('../assets/profile.png')}
           style={[
@@ -97,3 +83,152 @@ const styles = StyleSheet.create({
 });
 
 export default FooterMenu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+  
+
+// const FooterMenu = ({navigation}) => {
+//   const [activeTab, setActiveTab] = useState('home');
+
+
+//   const handleTabPress = (tab) => {
+//     setActiveTab(tab);
+
+//     // if (tab === 'home') {
+//     //   navigation.navigate('Homepage');   {/*  onPress={() => handleTabPress('home')} */}
+//     // } else if (tab === 'list') {
+//     //   navigation.navigate('List');
+//     // } else if (tab === 'profile') {    {/* onPress={() => handleTabPress('profile')}*/}
+//     //   navigation.navigate('Profile');
+//     // }   
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <TouchableOpacity
+//         style={styles.tab}
+   
+//       >   
+
+
+//         <Image
+//           source={require('../assets/home.png')}
+//           style={[
+//             styles.icon,
+//             activeTab === 'home' && styles.activeIcon,
+//           ]}
+//         />
+//         {activeTab === 'home' && <View style={styles.activeIndicator} />}
+//       </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.tab}
+//         onPress={() => handleTabPress('list')}
+//       >
+//         <Image
+//           source={require('../assets/lista.png')}
+//           style={[
+//             styles.icon,
+//             activeTab === 'list' && styles.activeIcon,
+//           ]}
+//         />
+//         {activeTab === 'list' && <View style={styles.activeIndicator} />}
+//       </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.tab}
+       
+//       >
+
+
+
+//         <Image
+//           source={require('../assets/profile.png')}
+//           style={[
+//             styles.icon,
+//             activeTab === 'profile' && styles.activeIcon,
+//           ]}
+//         />
+//         {activeTab === 'profile' && <View style={styles.activeIndicator} />}
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: 'row',
+//     backgroundColor: 'white',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//     height: 60,
+//     position: 'absolute',
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+//   },
+//   tab: {
+//     alignItems: 'center',
+//   },
+//   icon: {
+//     width: 24,
+//     height: 24,
+//   },
+//   activeIndicator: {
+//     width: 8,
+//     height: 8,
+//     borderRadius: 4,
+//     backgroundColor: '#807DFF',
+//     marginTop: 4,
+//   },
+// });
+
+// export default FooterMenu;
