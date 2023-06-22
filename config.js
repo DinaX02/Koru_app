f8caf8c423fda9f5c665aa6a95d23f643ed8d45f
githubTiagoMarques <al.tiagosoares@gmail.com>
2023-06-22 04:49:56 +0100

    api working

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://labmm.clients.ua.pt/proj/koru';
+export const BASE_URL = 'http://192.168.1.67';
--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,37 +1,30 @@
-import React, {createContext, useState} from 'react';
-import {BASE_URL} from "../config";
-import axios from "axios";
-
+import React, { createContext, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../config';
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-
+export const AuthProvider = ({ children }) => {
     const [userInfo, setUserInfo] = useState({});
 
     const login = (username, password) => {
-
         axios
             .post(`${BASE_URL}/user/login/${username}`, {
                 username,
                 password,
             })
             .then(res => {
-                let userInfo = res.data;
-                console.log(userInfo);
-                setUserInfo(userInfo);
-                alert("sucesso")
-                //AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+                const data = res.data;
+                const id_user = data.id_user;
+                const token = data.token;
+                alert(id_user);
+                // AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
             })
             .catch(e => {
                 console.log(`login error ${e}`);
-                alert("error")
+                alert(`login error ${e}`);
             });
     };
 
-
-    return(
-            <AuthContext.Provider value={{login}}>{children}</AuthContext.Provider>
-        );
-
-}
+    return <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>;
+};
