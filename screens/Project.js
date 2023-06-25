dc2b0b2e702049785dca024e8c70b9de4470a997
githubTiagoMarques <al.tiagosoares@gmail.com>
2023-06-25 23:27:09 +0100

    project page

---
+++ screens/Project.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, ImageBackground, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+const Project = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+            <Text>Project</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Project;
